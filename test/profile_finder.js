9b105569052d2f03e50653e0342522703909bcf8
Saad Tazi <stazi@fruitsoftware.com>
2024-04-15 08:50:53 -0400

    support ubuntu path (~/snap/...) (#139)
    * support ubuntu path (~/snap/...)
    
    * PR comments: prefer dotDir over XDG/.config
    
    * add warning if profile linux dir not found
    
    ---------
    
    Co-authored-by: Saad Tazi <saad.tazi@unity3d.com>

--- test/profile_finder.js
+++ test/profile_finder.js
@@ -2,91 +2,96 @@
 /*global sinon:false, expect, describe:false, it:false, before, beforeEach:false, afterEach:false*/
 
 var path = require('path'),
+  Finder = require('../lib/profile_finder'),
+  sandbox,
+  finder;
 
-    Finder = require('../lib/profile_finder'),
-    sandbox,
-    finder;
-
-
-describe('profile_finder', function() {
+describe('profile_finder', function () {
   'use strict';
 
-  beforeEach(function() {
+  beforeEach(function () {
     sandbox = sinon.sandbox.create();
   });
 
-  afterEach(function() {
+  afterEach(function () {
     sandbox.restore();
   });
 
-  describe('#constructor', function() {
-
-    beforeEach(function() {
+  describe('#constructor', function () {
+    beforeEach(function () {
       this.locateDirSpy = sandbox.spy(Finder, 'locateUserDirectory');
     });
 
-    it('should call Finder.locateUserDirectory if no param is provided', function() {
+    it('should call Finder.locateUserDirectory if no param is provided', function () {
       finder = new Finder();
       expect(this.locateDirSpy).to.be.calledOnce;
     });
-    it('should not call Finder.locateUserDirectory if a param is provided', function() {
+    it('should not call Finder.locateUserDirectory if a param is provided', function () {
       finder = new Finder('path/to/somewhere');
       expect(this.locateDirSpy).not.to.be.calledOnce;
     });
   });
 
-  describe('#Finder.locateUserDirectory', function() {
-    before(function() {
-      if (!process.env.HOME)    { process.env.HOME = '/somewhere'; }
-      if (!process.env.APPDATA) { process.env.APPDATA = '/somewhere'; }
+  describe('#Finder.locateUserDirectory', function () {
+    before(function () {
+      if (!process.env.HOME) {
+        process.env.HOME = '/somewhere';
+      }
+      if (!process.env.APPDATA) {
+        process.env.APPDATA = '/somewhere';
+      }
     });
     var tests = [
-      { platform: 'darwin', endsWith: /\/Library\/Application Support\/Firefox$/ },
-      { platform: 'linux',  endsWith: /\/.mozilla\/firefox$/ },
-      { platform: 'win32',  endsWith: /\/Mozilla\/Firefox$/ }
+      {
+        platform: 'darwin',
+        endsWith: /\/Library\/Application Support\/Firefox$/,
+      },
+      { platform: 'linux', endsWith: /\/.config\/mozilla\/firefox$/ },
+      { platform: 'win32', endsWith: /\/Mozilla\/Firefox$/ },
     ];
-    tests.forEach(function(test) {
-      it('should return path to user profile', function() {
-        expect(Finder.locateUserDirectory(test.platform)).to.match(test.endsWith);
+    tests.forEach(function (test) {
+      it('should return path to user profile', function () {
+        expect(Finder.locateUserDirectory(test.platform)).to.match(
+          test.endsWith
+        );
       });
-
     });
   });
 
-  describe('#readProfiles', function() {
-    it('should return only profiles from profiles.ini', function(done) {
+  describe('#readProfiles', function () {
+    it('should return only profiles from profiles.ini', function (done) {
       finder = new Finder(__dirname);
-      finder.readProfiles(function(err, profiles) {
+      finder.readProfiles(function (err, profiles) {
         expect(profiles).to.have.length(3);
         expect(profiles[0]).to.eql({
           Name: 'default',
           IsRelative: '1',
           Path: 'empty-profile',
-          Default: '1'
+          Default: '1',
         });
         done();
       });
     });
   });
 
-  describe('#getPath', function() {
-    beforeEach(function() {
+  describe('#getPath', function () {
+    beforeEach(function () {
       this.readProfilesSpy = sandbox.spy(Finder.prototype, 'readProfiles');
     });
 
-    it('should call readProfiles if it was not called before', function(done) {
+    it('should call readProfiles if it was not called before', function (done) {
       var self = this;
       finder = new Finder(__dirname);
-      finder.getPath('default', function() {
+      finder.getPath('default', function () {
         expect(self.readProfilesSpy).to.be.calledOnce;
         done();
       });
     });
 
-    it('should not call readProfiles if it was not called before', function(done) {
+    it('should not call readProfiles if it was not called before', function (done) {
       var self = this;
       finder = new Finder(__dirname);
-      finder.readProfiles(function() {
+      finder.readProfiles(function () {
         self.readProfilesSpy.reset();
         // no callback needed
         var dftProfile = finder.getPath('default');
@@ -94,19 +99,18 @@ describe('profile_finder', function() {
         expect(dftProfile).to.eql(path.join(__dirname, 'empty-profile'));
 
         // but you can for consistency...
-        finder.getPath('default', function(err, defaultProfile) {
+        finder.getPath('default', function (err, defaultProfile) {
           expect(self.readProfilesSpy).to.not.be.calledOnce;
           expect(defaultProfile).to.eql(path.join(__dirname, 'empty-profile'));
           done();
         });
-
       });
     });
 
-    it('should return a profile if it exists', function(done) {
+    it('should return a profile if it exists', function (done) {
       var self = this;
       finder = new Finder(__dirname);
-      finder.getPath('default', function() {
+      finder.getPath('default', function () {
         expect(self.readProfilesSpy).to.be.calledOnce;
         done();
       });
